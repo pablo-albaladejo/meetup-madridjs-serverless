904346e3517dc91ed675f879c5ee5bfb9ec78040
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2025-01-30 19:15:49 +0100

    chore(cdk): init typescript cdk app

---
+++ test/meetup-madridjs-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MeetupMadridjsServerless from '../lib/meetup-madridjs-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/meetup-madridjs-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MeetupMadridjsServerless.MeetupMadridjsServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
